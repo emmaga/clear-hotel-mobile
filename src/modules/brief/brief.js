de2681f1310d8a8d244f3aa778ddbdf760097bd1
kunmingren <834719922@qq.com>
2016-05-29 16:33:07 +0800

    brief、service-p1模板化加载

--- src/modules/brief/brief.js
+++ src/modules/brief/brief.js
@@ -1,19 +1,37 @@
-define(['framework7','appFunc','text!brief/brief.tpl.html'],
-    function(framework7,appFunc,template){
-    var myApp = new Framework7({});
-    var $$ = Dom7;
+define(['framework7','config', 'xhr','appFunc','text!brief/brief.tpl.html'],
+    function(framework7,config, xhr,appFunc,template){
+        var myApp = new Framework7({});
+        var $$ = Dom7;
 
-    var init = function (menuId){
-        var renderData = {};
-        var output = appFunc.renderTpl(template,renderData);
-        $$('#tab'+'brief'+'_'+menuId).html(output);
-        var mySwiper = myApp.swiper('.swiper-container', {
-            preloadImages: false,
-            lazyLoading: true,
-            pagination:'.swiper-pagination'
-        });
-    };
-    return {
-        init: init
-    };
-});
+
+        var brief = {
+            bindEvents: function() {
+
+            },
+            loadData: function(menuId,data) {
+                var renderData = data.brief;
+                var output = appFunc.renderTpl(template,renderData);
+                $$('#tab'+'brief'+'_'+menuId).html(output);
+                //初始化swiper
+                var mySwiper = myApp.swiper('.swiper-container', {
+                    preloadImages: false,
+                    lazyLoading: true,
+                    pagination:'.swiper-pagination'
+                });
+                brief.bindEvents();
+            }
+        }
+
+        var init = function (menuId){
+            xhr.ajax({
+                'url': config.getJSONUrl('brief'),
+                dataType: 'json',
+                'success': function(data){brief.loadData(menuId,data)}
+            });
+
+
+        };
+        return {
+            init: init
+        };
+    });
--- src/modules/service-p1/service-p1.js
+++ src/modules/service-p1/service-p1.js
@@ -5,16 +5,15 @@ define(['framework7', 'config', 'xhr','appFunc','text!service-p1/service-p1.tpl.
 
         var serviceP1 = {
             bindEvents: function() {
-                $$(document).on('click', '#menu-test', function (e) { 
+                $$(document).on('click', '#menu-test', function (e) {
                   serviceP2Module.init(serviceP1.menuId);
                 });
             },
-            loadData: function(data) {
+            loadData: function(menuId,data) {
                 var renderData = data.serviceP1;
                 var output = appFunc.renderTpl(template,renderData);
                 $$('#tab'+'service-p1'+'_'+menuId).html(output);
-
-                serviceP1.menuId = menuId;
+                //serviceP1.menuId = menuId;   获取不到data，将menuId作为参数传递
                 serviceP1.bindEvents();
             }
         }
@@ -23,7 +22,7 @@ define(['framework7', 'config', 'xhr','appFunc','text!service-p1/service-p1.tpl.
             xhr.ajax({
                 'url': config.getJSONUrl('serviceP1'),
                 dataType: 'json',
-                'success': function(data){serviceP1.loadData(data)}
+                'success': function(data){serviceP1.loadData(menuId,data)}
             })
 
         };
