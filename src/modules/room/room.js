0cb210328c9f73155ac5c00b400fe1e630ae77ce
kunmingren <834719922@qq.com>
2016-06-08 10:14:46 +0800

    完成日历选择日期的逻辑

--- src/modules/room/room.js
+++ src/modules/room/room.js
@@ -3,13 +3,17 @@ define(['framework7','appFunc','text!room/room.tpl.html'], function(framework7,a
     var $$ = Dom7;
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 'September' , 'October', 'November', 'December'];
 
+    var room = {
+
+    }
+    var confirm = [];
+
     var init = function (menuId){
         var renderData = {};
         var output = appFunc.renderTpl(template,renderData);
         $$('#tab_'+'room'+'_'+menuId).html(output);
         //日历控件
         var today = new Date();
-        var weekLater = new Date().setDate(today.getDate() + 7);
         var tomorrow = new Date().setDate(today.getDate() + 1);
         var yesterday = new Date().setDate(today.getDate() - 1);
 
@@ -18,26 +22,20 @@ define(['framework7','appFunc','text!room/room.tpl.html'], function(framework7,a
             dateFormat: 'M dd yyyy',
             onlyOnPopover:true,
             toolbar:true,
-            toolbarCloseText:'OK',
             multiple:true,
             rangesClasses: [
-                //Add "day-october' class for all october dates
                 {
-                    // string CSS class name for this range in "cssClass" property
-                    cssClass: 'picker-calendar-day-selected', //string CSS class
-                    // Date Range in "range" property
+                    cssClass: 'picker-calendar-day-selected',
                     range: {
                         from: yesterday,
                         to: tomorrow
                     }
                 }],
-            //convertToPopover:true,
-            //onlyOnPopover:true,
             toolbarTemplate:
             '<div class="toolbar calendar-custom-toolbar">' +
                 '<div class="toolbar-inner">' +
                     '<div class="left">' +
-                        '<a href="#" class="link icon-only"><i class="icon icon-back"></i></a>' +
+                        '<a href="#" class="link icon-only close-picker"><i class="icon icon-back"></i></a>' +
                     '</div>' +
                         '<a href="#" class="link icon-only picker-calendar-prev-month">'+
                             '<i class="icon icon-prev"></i>'+
@@ -47,7 +45,7 @@ define(['framework7','appFunc','text!room/room.tpl.html'], function(framework7,a
                             '<i class="icon icon-next"></i>'+
                         '</a>'+
                     '<div class="right">' +
-                        '<a href="#" class="link close-picker">{{closeText}}&nbsp;&nbsp;</a>' +
+                        '<a href="#" class="link">OK&nbsp;&nbsp;</a>' +
                     '</div>' +
                 '</div>' +
             '</div>',
@@ -55,31 +53,45 @@ define(['framework7','appFunc','text!room/room.tpl.html'], function(framework7,a
                 //var price = '<div data-year="2016" data-month="5" data-day="1" class="picker-calendar-day picker-calendar-day-selected" data-date="2016-5-1" style="position:relative"><span class="">1</span><br><div style="position:absolute;top:60px;left:0;width:100%;text-align:center;margin:0 auto;">$100</div></div>';
 
                 $$('.picker-calendar').css('height',"100%");
-                //calendarDisabled.setPrice();
                 $$('.calendar-custom-toolbar .current-value').text(monthNames[p.currentMonth] +', ' + p.currentYear);
-                //$$('.calendar-custom-toolbar .picker-calendar-prev-month').on('click', function () {
-                //    calendarDisabled.prevMonth();
-                //});
-                //$$('.calendar-custom-toolbar .picker-calendar-next-month').on('click', function () {
-                //    calendarDisabled.nextMonth();
-                //});
                 $$('.calendar-custom-toolbar .right').on('click', function (){
-                    var oldTime = calendarDisabled.value;
-                    var chooseValue = new Date(parseInt(oldTime));
-                    alert(chooseValue);
+                    //var oldTime = appFunc.dateToTime('2016-5-9');
+                    //alert(oldTime);
+                    //var daySelected = $$('.picker-calendar-day-selected');
+                    if(confirm.length==0){
+                        //传参为默认的today和tomorrow
+                        today = today.getTime();
+                        var dateIn =  appFunc.timeToDate(today,'yyyy/MM/dd');
+                        var dateOut = appFunc.timeToDate(tomorrow,'yyyy/MM/dd')
+
+                        console.log(dateIn+"++++++++"+dateOut)
+                    }
+                    else if(confirm.length==2){
+                        confirm.sort();
+                        var earlyDate = confirm[0];
+                        var lateDate = confirm[1];
+                        var dateIn =  appFunc.timeToDate(earlyDate,'yyyy/MM/dd');
+                        var dateOut = appFunc.timeToDate(lateDate,'yyyy/MM/dd')
+                        console.log(dateIn+'+++++++++'+dateOut);
+                    }else{
+                        alert("请选择入住和离店时间");
+                    }
                 });
             },
             onMonthYearChangeStart: function (p) {
                 $$('.calendar-custom-toolbar .current-value').text(monthNames[p.currentMonth] +', ' + p.currentYear);
             },
-            onDayClick:function(p){
-                var daySelected = $$('.picker-calendar-day-selected');
-                if(daySelected.length>=2) {
+            onChange:function(p,values){
+                //var daySelected = $$('.picker-calendar-day-selected');
+                var result = calendarDisabled.value;
+                confirm = result;
+                if(result.length>=2) {
                     calendarDisabled.value = [];
-                }else{
-                    
                 }
             }
+            //onDayClick:function(p, dayContainer, year, month, day){
+            //    console.log(day)
+            //}
 });
     };
     return {
