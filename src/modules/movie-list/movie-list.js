45c6a18c41ee8efd6ccf4014ee4b25dba6d5df8d
kunmingren <834719922@qq.com>
2016-06-21 17:58:09 +0800

    appid

--- src/modules/movie-list/movie-list.js
+++ src/modules/movie-list/movie-list.js
@@ -32,8 +32,9 @@ define(['framework7','config', 'xhr', 'appFunc', 'router', 'text!movie-list/movi
                 var loading = false;
                 var html = '';
                 var pageSize = infData.length < 10 ? infData.length : 10;
+                var appId = config.getAppId();
                 for (var i = 0; i < pageSize; i++) {
-                    html += "<a href='movie-list-detail.html?moduleId="+moduleId+"&movieId="+infData[i].movieId+"' class='col-100'><div class='movie-list'><img class='lazy movie-p1-img' src='"+infData[i].imgUrl+"'><h3 class='movie-p1-h3'>"+infData[i].name+"</h3><p class='movie-p1-p1'>"+infData[i].intro1+"</p><p class='movie-p1-p2'>"+infData[i].intro2+"</p> </div></a>";
+                    html += "<a href='movie-list-detail.html?appId="+appId+"&moduleId="+moduleId+"&movieId="+infData[i].movieId+"' class='col-100'><div class='movie-list'><img class='lazy movie-p1-img' src='"+infData[i].imgUrl+"'><h3 class='movie-p1-h3'>"+infData[i].name+"</h3><p class='movie-p1-p1'>"+infData[i].intro1+"</p><p class='movie-p1-p2'>"+infData[i].intro2+"</p> </div></a>";
                 }
                 // 添加新条目
                 $$('#movie-list-row_'+moduleId).append(html);
@@ -67,7 +68,7 @@ define(['framework7','config', 'xhr', 'appFunc', 'router', 'text!movie-list/movi
                     var html = '';
                     for (var i = lastIndex ; i < lastIndex + itemsPerLoad; i++) {
                         //console.log(infData[i].movieId)
-                        html += "<a href='movie-p2.html?movieId="+infData[i].movieId+"' class='col-100'><div class='movie-list' data-movieId='"+infData[i].movieId+"'><img class='lazy movie-p1-img' src='"+infData[i].imgUrl+"'><h3 class='movie-p1-h3'>"+infData[i].name+"</h3><p class='movie-p1-p1'>"+infData[i].intro1+"</p><p class='movie-p1-p2'>"+infData[i].intro2+"</p> </div></a>";
+                        html += "<a href='movie-p2.html?appId="+appId+"&movieId="+infData[i].movieId+"' class='col-100'><div class='movie-list' data-movieId='"+infData[i].movieId+"'><img class='lazy movie-p1-img' src='"+infData[i].imgUrl+"'><h3 class='movie-p1-h3'>"+infData[i].name+"</h3><p class='movie-p1-p1'>"+infData[i].intro1+"</p><p class='movie-p1-p2'>"+infData[i].intro2+"</p> </div></a>";
                     }
 
                     // 添加新条目
